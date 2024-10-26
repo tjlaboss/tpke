a5f132964b5d922b34f3d525e0633b0f29848096
Travis L. <travisl2@illinois.edu>
2024-10-26 16:09:52 -0600

    Start adding main module

---
+++ tpke/__main__.py
@@ -0,0 +1,24 @@
+"""
+Travis's Point Kinetics Equations
+"""
+import tpke
+
+import os
+
+
+def main():
+	args = tpke.arguments.get_arguments()
+	input_file = os.path.abspath(args.input_file)
+	if not os.path.isfile(input_file):
+		raise FileNotFoundError(input_file)
+	input_dict = tpke.yamlin.load_input_file(input_file)
+	if args.yaml_validate:
+		# If not, we would have errored out above.
+		print("Input file is valid:", input_file)
+		exit(0)
+	# Todo: implement the other stuff.
+	print(input_dict)
+
+
+if __name__ == "__main__":
+	exit(main())
