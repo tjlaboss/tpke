6d2731350c33dd42aeaac575820048d1c4e1ea42
Travis L. <travisl2@illinois.edu>
2024-10-26 15:00:24 -0600

    Build Explicit Euler

--- tpke/matrices.py
+++ tpke/matrices.py
@@ -105,3 +105,85 @@ def implicit_euler(
         B[n*(k+2)-1] = C0s[k]
     return A, B
 
+
+def explicit_euler(
+        n: int,
+        rho_vec: _T_arr,
+        dt: float,
+        betas: _T_arr,
+        lams: _T_arr,
+        L: float,
+        P0: float = 1,
+) -> typing.Tuple[_T_arr, _T_arr]:
+    """Build A and B matrices using Explicit Euler.
+
+    Example for 1 delayed group:
+
+        [-1 - dt*(rho - beta)/L] P_n  +  [+1] P_{n+1}  + [-dt*lambda_k] C_{k,n}                    = 0
+        [+1]                     P_0                                                               = P0
+        [-dt*beta_k/Lambda]      P_n  +              [-1 + dt*lambda_k] C_{k,n}  +  [+1] C_{k,n+1} = 0
+                                                                   [+1] C_{k,n}                    = C0_k
+
+
+    Parameters:
+    -----------
+    n: int
+        Number of timesteps
+
+    rho_vec: np.ndarray(float)
+        Array of reactivities at each timestep.
+
+    dt: float
+        Timestep size (s).
+
+    betas: np.ndarray(float)
+        Array of delayed neutron precursor fission yields.
+
+    lams: np.ndarray(float)
+        Array of delayed neutron precursor decay constants (s^-1).
+
+    L: float
+        Prompt neutron lifetime (s).
+
+    P0: float, optional.
+        Starting power.
+        [Default: 1]
+
+    Returns:
+    --------
+    A: np.ndarray
+        Square [NxN] array, for LHS of matrix solution.
+
+    B: np.ndarray
+        Vector [Nx1] array, for RHS of matrix solution.
+    """
+    __check_inputs(n, rho_vec, betas, lams)
+    ndg = len(betas)  # number of delayed groups
+    beff = sum(betas)  # beta effective
+    size = (1 + ndg)*n
+    A = np.zeros((size, size))
+    B = np.zeros(size)
+    C0s = (P0*betas)/(lams*L)  # Initial precursor concentrations
+    for ip in range(n - 1):
+        rho0 = rho_vec[ip]
+        dtrbl = dt*(rho0 - beff)/L
+        dtl = dt*beff/L
+        # P, normal nodes
+        A[ip, ip] = -1 - dtrbl  # P_n
+        A[ip, ip + 1] = 1       # P_{n+1}
+        for k in range(ndg):
+            ic = ip + n*(k + 1)
+            A[ip, ic] = -dt*lams[k]         # C_{k,n}
+            # C, normal nodes
+            A[ic, ip + 1] = -dtl            # P_{n+1}
+            A[ic, ic] = -1 + dt*lams[k]     # C_{k,n}
+            A[ic, ic + 1] = 1               # next C
+    # Boundary Conditions
+    # Initial Condition: P
+    A[n - 1, 0] = 1
+    B[n - 1] = P0
+    # Initial Condition: C
+    for k in range(ndg):
+        A[n*(k + 2) - 1, n*(k + 1)] = 1
+        B[n*(k + 2) - 1] = C0s[k]
+    return A, B
