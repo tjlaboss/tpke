e5099a9e850bf3aa26875a7fe1332979139d7955
Travis L. <travisl2@illinois.edu>
2024-10-27 14:28:33 -0600

    Finish up plotting mode behavior

--- tpke/__main__.py
+++ tpke/__main__.py
@@ -93,30 +93,36 @@ def plot_only(output_dir: tpke.tping.PathType):
 			errs.append(f"Failed to plot Matrix A: {type(e)}: {e}")
 		else:
 			plt.savefig(K.FNAME_SPY)
+			print("Matrix A spy plot saved to:", K.FNAME_SPY)
 	# Power-Reactivity plot
+	tfpath = os.path.join(output_dir, K.FNAME_TIME)
 	rfpath = os.path.join(output_dir, K.FNAME_RHO)
 	pfpath = os.path.join(output_dir, K.FNAME_P)
-	if not os.path.exists(rfpath):
+	if not os.path.exists(tfpath):
+		errs.append(f"Times could not be found at: {tfpath}")
+	elif not os.path.exists(rfpath):
 		errs.append(f"Reactivities could not be found at: {rfpath}")
 	elif not os.path.exists(pfpath):
 		errs.append(f"Reactor powers could not be found at: {pfpath}")
 	else:
 		try:
+			times = np.loadtxt(tfpath)
 			reactivities = np.loadtxt(rfpath)
 			powers = np.loadtxt(pfpath)
-			# and plot...
+			# if len(times) != len(reactivities) != len(powers)  -> handled in plotting
+			tpke.plotter.plot_reactivity_and_power(times, reactivities, powers)
 		except Exception as e:
 			errs.append(f"Failed to plot power and reactivity: {type(e)}: {e}")
 		else:
-			pass
-			#plt.savefig()
-	if errs:
-		le = len(errs)
+			plt.savefig(K.FNAME_PR)
+			print("Power and reactivity plot saved to:", K.FNAME_PR)
+	le = len(errs)
+	if le:
 		errstr = f"There were {le} errors:\n\t"
 		errstr += "\n\t".join(errs)
 		print(errs, sys.stderr)
-		exit(le)
-	exit(0)
+	plt.show()
+	exit(le)
 
 
 def main():
