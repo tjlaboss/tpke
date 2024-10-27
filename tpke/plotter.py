f276071464243acf36ce2d6e456fc4f47cb36c0e
Travis L. <travisl2@illinois.edu>
2024-10-26 19:06:52 -0600

    Plot reactivity and power

--- tpke/plotter.py
+++ tpke/plotter.py
@@ -9,16 +9,20 @@ import typing
 
 V_float = typing.Collection[float]
 
+COLOR_P = "forestgreen"
+COLOR_R = "firebrick"
+
 
 def show():
 	return plt.show()
 
 
-def plot_power_and_reactivity(
+def plot_reactivity_and_power(
 		times: V_float,
 		reacts: V_float,
 		powers: V_float,
-		power_units=None
+		power_units=None,
+		title_text=""
 ):
 	"""Plot the reactor power and reactivity vs. time
 	
@@ -33,9 +37,13 @@ def plot_power_and_reactivity(
 	powers: collection of float
 		List of powers (power_units).
 	
-	power_units: str
+	power_units: str, optional
 		Units to show for the y-axis for power.
 		[Default: None --> relative power]
+	
+	title_text: str, optional
+		Title for the plot.
+		[Default: None]
 	"""
 	n = len(times)
 	len_p = len(powers)
@@ -44,7 +52,28 @@ def plot_power_and_reactivity(
 		f"The number of times ({n}), powers ({len_p}), and reactivities ({len_r}) must be equal."
 	if power_units is None:
 		power_units = "Relative"
+	
+	# Plot power
+	fig, pax = plt.subplots()
+	plines = pax.plot(times, powers, "-", color=COLOR_P, label=r"$P(t)$")
+	pax.tick_params(axis="y", labelcolor=COLOR_P)
+	pax.set_ylabel(f"Power ({power_units})", color=COLOR_P)
+	
+	# Plot reactivity
+	rax = pax.twinx()
+	rlines = rax.plot(times, reacts, "--", color=COLOR_R, label=r"$\rho(t)$")
+	rax.tick_params(axis="y", labelcolor=COLOR_R)
+	rax.set_ylabel("Reactivity (\$)", color=COLOR_R)
+	
+	lines = plines + rlines
+	pax.legend(lines, [l.get_label() for l in lines], loc=0)
+	pax.set_xlim([0, max(times)])
+	pax.set_xlabel("Time (s)")
+	
 	# continue...
+	if title_text:
+		plt.suptitle(title_text)
+	plt.tight_layout()
 
 
 def plot_matrices(matA):
