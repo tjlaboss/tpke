4e63046814231d2438a78b64284d6bf1b3aade12
Travis L. <travisl2@illinois.edu>
2024-10-26 19:51:55 -0600

    Boilerplate

--- tpke/__init__.py
+++ tpke/__init__.py
@@ -1,6 +1,9 @@
 from warnings import simplefilter as _simplefilter
 _simplefilter("ignore", UserWarning)
 
+__author__ = "Travis J. Labossiere-Hickman"
+__email__ = "travisl2@illinois.edu"
+
 import tpke.arguments
 import tpke.matrices
 import tpke.reactivity
