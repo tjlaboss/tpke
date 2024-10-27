121bcd2618786d4264cbde40de7022904ec3aafc
Travis L. <travisl2@illinois.edu>
2024-10-26 19:49:00 -0600

    Add plotting to main

--- tpke/__main__.py
+++ tpke/__main__.py
@@ -5,11 +5,13 @@ import typing
 import tpke
 import os
 import numpy as np
+import warnings
 
 np.set_printoptions(legacy='1.25', linewidth=np.inf)
 
 
 def solution(input_dict: typing.Mapping):
+	plots = input_dict.get('plots', {})
 	method = tpke.matrices.METHODS[input_dict['method']]
 	total = input_dict['time']['total']
 	dt = input_dict['time']['dt']
@@ -31,9 +33,28 @@ def solution(input_dict: typing.Mapping):
 		L=input_dict['data']['Lambda'],
 		rho_vec=reactivity_vals.copy()
 	)
+	to_show = plots.get('show', 0)
+	if plots.get('spy'):
+		tpke.plotter.plot_matrices(matA)
+		if to_show > 1:
+			tpke.plotter.show()
 	power_vals, concentration_vals = tpke.solver.linalg(matA, matB, num_steps)
 	# print(np.vstack((power_vals, concentration_vals)))
 	print(power_vals)
+	prplot = plots.get('power_reactivity')
+	times = np.linspace(0, num_steps*dt, num_steps)
+	if prplot == 1:
+		tpke.plotter.plot_reactivity_and_power(times, reactivity_vals, power_vals)
+	elif prplot == 2:
+		# Plot them separately
+		warnings.warn("Not implemented yet: separate power and reactivity plots", FutureWarning)
+	if to_show > 1:
+		tpke.plotter.show()
+	
+	# keep at end
+	if to_show:
+		tpke.plotter.show()
+		
 
 
 def main():
